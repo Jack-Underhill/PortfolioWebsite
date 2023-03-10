d21fc78add5eb466284a3473bc246671010c4401
Jack-Underhill <jack.underhill17@gmail.com>
2023-03-10 02:39:33 -0800

    Finishing touches before presentation

---
+++ js/test.js
@@ -0,0 +1,12 @@
+// const navigation = document.querySelector("header");
+
+// const navigationHeight = navigation.offsetHeight;
+
+// const pageHeight = document.querySelector("header");
+
+// const resultHeight = navigationHeight;
+
+// document.getElementsByClassName('page').style.setProperty(
+//   "max-height",
+//   navigationHeight + "px"
+// );
